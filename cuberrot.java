7c2aa13e92c7dfc1daae7400acb27b27f9d89f5d
vaishnavichittepu18-afk <vaishnavichittepu18@gmail.com>
2026-06-27 20:01:22 +0530

    Create cuberrot.java

---
+++ cuberrot.java
@@ -0,0 +1,16 @@
+long cubeRoot(long n){
+    long l = 0;
+    long r = 1000000;//0<=n<=10^18(since n is long) ,so if r=n overflow may occur and max possible cuberoot=cuberroot(10^18)=10^6
+    while(l <= r){
+        long m = l + (r - l) / 2;
+        long val = m * m * m;
+        if(val == n)
+            return m;
+        else if(val < n)
+            l = m + 1;
+        else
+            r = m - 1;
+    }
+    return -1;
+}
+//Time: O(logn)
